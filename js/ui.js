66d9a646ec0978d467b3812a185098e8b4e5b787
Anonymous <159781143+4n0nymou3@users.noreply.github.com>
2026-08-14 02:38:53 +0330

    ui.js

--- js/ui.js
+++ js/ui.js
@@ -69,7 +69,9 @@ export function escapeHtml(str) {
   return String(str)
     .replace(/&/g, '&amp;')
     .replace(/</g, '&lt;')
-    .replace(/>/g, '&gt;');
+    .replace(/>/g, '&gt;')
+    .replace(/"/g, '&quot;')
+    .replace(/'/g, '&#39;');
 }
 
 export function highlightJsonLine(line) {
